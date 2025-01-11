404c467890ce6103995b5aec3d23a159a3ea28ca
chrisvgividen <c.v.givich@gmail.com>
2025-01-11 11:45:11 -0700

    Finished part 2 of today's assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        /*Console.WriteLine("Hello Prep2 World!"); */
+
+        Console.Write("What is your final grade percentage? ");
+        string finalgrade = Console.ReadLine();
+        int grade = int.Parse(finalgrade);
+
+        string gradeletter = "";
+
+        if (grade >= 90)
+        {
+            gradeletter = "A";
+        }
+        else if (grade >= 80)
+        {
+            gradeletter = "B";
+        }
+        else if (grade >= 70)
+        {
+            gradeletter = "C";
+        }
+        else if (grade >= 60)
+        {
+            gradeletter = "D";
+        }
+        else
+        {
+            gradeletter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {gradeletter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
+    
 }
