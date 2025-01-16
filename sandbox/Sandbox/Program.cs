dd7df6d6398f797337e02c538f0096d06f8ad6df
Christian Gividen <c.v.givich@gmail.com>
2025-01-16 16:19:58 -0700

    Finished sandbox cs program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // comment, or
+        /* this comment */
+        string adjective = GetAdjective();
+        string noun = GetNoun();
+        int number = Multiply(3, 4);
+
+        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}");
+    }
+
+    static int Multiply( int number1, int number2)
+    {
+        int product = number1 * number2;
+        return product;
+    }
+
+    static string GetAdjective()
+    {
+        List<string> words = new List<string>();
+        words.Add("blue");
+        words.Add("red");
+        words.Add("orange");
+        words.Add("purple");
+
+        string adjective = words [2];
+
+
+        // string adjective = "yellow";
+        return adjective;
+    }
+
+    static string GetNoun()
+    {
+        string word = "bird";
+        return word;
     }
 }
