3fa5331d058cd2cb797e41eed659299b2b6887f7
Christian Gividen <c.v.givich@gmail.com>
2025-01-18 17:52:33 -0700

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != randomNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+        }
     }
+
+
 }
