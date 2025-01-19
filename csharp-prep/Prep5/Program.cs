808530643f4dbc3f1cdd8b575dfe83cd277dcea9
Christian Gividen <c.v.givich@gmail.com>
2025-01-18 18:46:37 -0700

    Finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Console.WriteLine("Hello Prep5 World!");
+
+        WelcomeMessage();
+
+        string name = UserName();
+
+        int number = UserFavoriteNumber();
+
+        int squaredNum = FavoriteNumberSquared(number);
+
+        EndScreen(name, squaredNum);
+
+    }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to this progoram!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("What is your name?: ");
+        string nameOfUser = Console.ReadLine();
+
+        return nameOfUser;
+    }
+
+    static int UserFavoriteNumber()
+    {
+        Console.Write("What is your favorite number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+
+        return userNumber;
+    }
+
+    static int FavoriteNumberSquared(int favoriteNumber)
+    {
+        int squared = favoriteNumber * favoriteNumber;
+
+        return squared;
+    }
+
+    static void EndScreen(string nameOfUser, int squared)
+    {
+        Console.WriteLine($"Hey {nameOfUser}! Your favorite number squared is {squared}");
     }
 }
