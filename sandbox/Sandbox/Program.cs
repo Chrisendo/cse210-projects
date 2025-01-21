f0d839adf0d8ea7d76693a03ef3f119e554438e8
Christian Gividen <c.v.givich@gmail.com>
2025-01-21 16:19:47 -0700

    Created Movie.cs, and created both a Movie class and a void item in that file. I then used both of these "functions" to call upon two movies and their data. Loved every moment of this class.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,42 +1,76 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // comment, or
+        //int name = "john";
+        Movie favoriteMovie = new Movie();
+        favoriteMovie._title = "Star Wars";
+        favoriteMovie._year = 1977;
+        favoriteMovie._rating = "PG";
+        favoriteMovie._runtime = 150;
+        
+        Movie otherMovie = new Movie();
+        otherMovie._title = "Avatar";
+        otherMovie._year = 2009;
+        otherMovie._runtime = 162;
+        otherMovie._rating = "PG-13";
+        
+        // Console.WriteLine(favoriteMovie._title);
+
+        favoriteMovie.Display();
+        otherMovie.Display();
+
+        DisplayMovie(favoriteMovie);
+        DisplayMovie(otherMovie);
+    }
+
+    static void DisplayMovie(Movie aMovie)
+    {
+        Console.WriteLine($"{aMovie._title} - {aMovie._year}");
+    }
+}
+
+
+
+
+
+
+
+/*
+// comment, or
         /* this comment */
-        string adjective = GetAdjective();
-        string noun = GetNoun();
-        int number = Multiply(3, 4);
+       // string adjective = GetAdjective();
+       // string noun = GetNoun();
+       // int number = Multiply(3, 4);
 
-        Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}");
-    }
+       // Console.WriteLine($"I looked out the window and saw {number} {adjective} {noun}");
+   // }
 
-    static int Multiply( int number1, int number2)
-    {
-        int product = number1 * number2;
-        return product;
-    }
+    //static int Multiply( int number1, int number2)
+   // {
+     //   int product = number1 * number2;
+     //   return product;
+   // }
 
-    static string GetAdjective()
-    {
-        List<string> words = new List<string>();
-        words.Add("blue");
-        words.Add("red");
-        words.Add("orange");
-        words.Add("purple");
+  //  static string GetAdjective()
+   // {
+      //  List<string> words = new List<string>();
+      //  words.Add("blue");
+      //  words.Add("red");
+      //  words.Add("orange");
+      //  words.Add("purple");
 
-        string adjective = words [2];
+       // string adjective = words [2];
 
 
         // string adjective = "yellow";
-        return adjective;
-    }
+      //  return adjective;
+   // }
 
-    static string GetNoun()
-    {
-        string word = "bird";
-        return word;
-    }
-}
+    //static string GetNoun()
+  //  {
+     //   string word = "bird";
+     //   return word;
